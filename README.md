1d712ac9069a4cbebd04496d2ddf71369c2fbae2
Andy Hawkes <andy.hawkes@deltatre.com>
2024-02-26 10:53:28 +0000

    Intial commit of work clock code

---
+++ README.md
@@ -0,0 +1,80 @@
+# Work clock
+
+A simple word-based clock built for a Raspberry Pi Zero with the InkypHAT e-ink display.
+
+## Inky pHAT
+
+I bought a [black/white/red Inky pHAT from the Pi Hut](https://thepihut.com/products/inky-phat) - due to an issue with the first one they sent out not refreshing I had to get it replaced, but the customer service was excellent throughout.
+
+I'm running mine on a [Raspberry Pi Zero 2 W](https://thepihut.com/products/raspberry-pi-zero-2) (also bought from the Pi Hut) - I've got several Pi Zero W's knocking about in various states of project use, but I fancied a new one! They didn't have [the ones with the fancy coloured headers pre-soldered](https://thepihut.com/products/raspberry-pi-zero-2?variant=41181426942147), so I had to solder my own in.
+
+# Overview
+
+Inspired by [Matt Webb's AI Poem clock](https://www.kickstarter.com/projects/genmon/poem-1-the-ai-poetry-clock) as well as this [very expensive wall clock](https://www.jurawatches.co.uk/products/qlocktwo-classic-creators-edition-rust-wall-clock-45cm-fcenrt) (and this [much less expensive one](https://amzn.to/3SPwldn)), I wanted a word-based clock for my office that would not show the exact time but would be deliberately more vague.
+
+![It's half past nine](/images/half-past.jpg)
+![It's twenty-five to ten](/images/25-to.jpg)
+![Bang on 4 o'clock](/images/on-the-hour.jpg)
+
+An e-paper display seemed ideal for this as it would only need to update relatively infrequently (every 5 minutes).
+
+Because this was going to be sat on my desk, I only want / need it to show the time during by working hours (and to act as a prompt to tell me to stop working at the appropriate hour), so to reduce the number of [display refreshes*](#caveats) the script will write a status value to a text file (`work-clock-status.txt`) each time it runs, and will check the status next time it runs in order to see if it needs to update the display or not.
+
+You will need to update the following value in your `work-clock.py` file:
+
+```
+status_file = "/path/to/your/script/work-clock-status.txt"
+```
+
+You will need to use the full path, as the clock will ultimately be run by a [cron job](#automation).
+
+By default, the clock will only update between 09:00 and 18:00, Monday to Friday - this can be edited in [lines 31 to 34 of `work-clock.py`](work-clock.py#L31-34) but [this will eventually be made configurable via an argument when running the script](#to-do).
+
+Outside of those times it will display either:
+
+![It's not work time now](/images/not-work-time.jpg)
+
+or
+
+![It's the weekend!](/images/weekend.jpg)
+
+
+# Usage
+
+The script should be run with Python 3.
+
+```
+python3 work-clock.py
+```
+
+There are two optional parameters that can be passed in to make the configuration easier:
+
+* `--timezone, -t` - a [timezone string](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones) e.g. "Eurpoe/Prague" Default is `Europe/London`.
+* `--rotation, -r` - a rotation value for the screen in degrees. On a Pi Zero 90 = USB's at the top, 270 = USBs at the bottom. Default is `90`.
+
+## Automation
+
+Running the script via Python 3 will update the display once, so if you want to run it as an actual clock you will need to set up a `cron` job to run every 5 minutes.
+
+This is pretty simple - just run `crontab -e` and enter the following:
+
+```
+*/5 * * * * python3 /path/to/your/script/work-clock.py
+```
+
+Obviously you will need to substitute the correct path to the file on your device.
+
+# Caveats
+
+I believe that the Inky pHAT e-paper display is rated for about 100,000 refresh cycles - this means that if the clock script refreshes the display 12 times an hour, 9 hours per day, 5 days a week, it will potentially burn out the display in around 185 weeks (three and a half years).
+
+If it was running every 5 minutes without non-working / weekend days then it would burn it out in just under a year!
+
+# Acknowledgements
+
+Hat tip to the good people at Pimoroni for the [Inky Python library](https://github.com/pimoroni/inky) and their [excellent tutorial on getting started with the Inky pHAT](https://learn.pimoroni.com/article/getting-started-with-inky-phat).
+
+# To do
+
+* Add starting and ending times to the supported arguments to make them easily configurable.
+* Add non-working days to the supported arguments to make them easily configurable.
---
+++ work-clock.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from inky.auto import auto
+from PIL import Image, ImageFont, ImageDraw
+from font_fredoka_one import FredokaOne
+from datetime import datetime
+import pytz
+import inflect
+import argparse
+import sys
+
+# Get arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--timezone', '-t', type=str, required=False, help="Your desired timezone e.g. Europe/London")
+parser.add_argument('--rotation', '-r', type=str, required=False, help="The rotation of your screen e.g. 270. Defaults to 90")
+args, _ = parser.parse_known_args()
+
+# Set screen rotation from "rotation" argument or fall back to default
+# On Pi Zero W, 90 = USB's at the top, 270 = USBs at the bottom
+rotation = args.rotation or 90
+
+# Set timezone from "timezone" argument or fall back to default
+timezone = args.timezone or 'Europe/London'
+
+# Get local time
+tz = pytz.timezone(timezone)
+localtime = datetime.now(tz)
+
+# Define non-work days
+weekend_days = {5,6}
+# Define work day start and end times (hours)
+workday_start = 9
+workday_end = 18
+
+# Location of status file containing clock status from last execution
+status_file = "/path/to/your/script/work-clock-status.txt"
+
+# Set up Inky pHAT
+board = auto()
+
+# Define available colours
+red=board.RED
+black=board.BLACK
+white=board.WHITE
+
+# Define basic layout
+board.set_border(white)
+board.rotation = rotation
+padding = 10
+
+# Use inflect to turn numbers into words
+inflection = inflect.engine()
+
+# Prepare a canvas
+img = Image.new('P', (board.WIDTH, board.HEIGHT))
+draw = ImageDraw.Draw(img)
+
+# Define font options
+big_font = ImageFont.truetype(FredokaOne, 30)
+small_font = ImageFont.truetype(FredokaOne, 24)
+
+# Output aligned text
+def alignText(h="l", v="t", message='DEBUG', font=small_font, colour=black):
+    mw, mh = font.getsize(message)
+
+    # if an integer is passed for the h (horizontal) position then use it
+    if isinstance(h, int):
+        x = h
+    # otherwise align left (l - default), middle (m) or right (r) 
+    elif h == "r":
+        x = board.WIDTH - mw - padding
+    elif h == "m":
+        x = (board.WIDTH / 2) - (mw / 2)
+    else:
+        x = padding
+
+    # if an integer is passed for the v (vertical) position then use it
+    if isinstance(v, int):
+        y = v
+    # otherwise align top (t - default), middle (m), or bottom(b)
+    elif v == "b":
+        y = ((board.HEIGHT / 3) * 2.5) - (mh / 2) - padding
+    elif v == "m":
+        y = ((board.HEIGHT / 3) * 1.5) - (mh / 2)
+    else:
+        y = ((board.HEIGHT / 3) * 0.5) - (mh / 2) + padding
+
+    draw.text((x, y), message, fill=colour, font=font)
+
+# Get current hour and minutes
+hour24 = localtime.hour
+mins = localtime.minute
+
+# Get clock status from last execution
+with open(status_file, "r") as f:
+    clock_status = f.read()
+
+# We don't want to update the display too often to avoid shortening the lifespan
+# Check if it's the weekend
+# We only update once during the weekend
+if localtime.weekday() in weekend_days:
+    if clock_status != "weekend":
+        with open(status_file, "w") as f:
+            f.write("weekend")
+        alignText("m", "t", "It's", big_font)
+        alignText("m", "m", "the", big_font)
+        alignText("m", "b", "weekend!", big_font)
+        board.set_image(img)
+        board.show()
+        print("It's the weekend!")
+    sys.exit()
+# We only update once during non-working hours
+elif hour24 < workday_start or hour24 >= workday_end:
+    if clock_status != "not working":
+        with open(status_file, "w") as f:
+            f.write("not working")
+        alignText("l", "t", "It's")
+        alignText("m", "t", "not", big_font, red)
+        alignText("m", "m", "work")
+        alignText("m", "b", "time")
+        alignText("r", "b", "now")
+        board.set_image(img)
+        board.show()
+        print("It's not work time now")
+    sys.exit()
+# We update when needed during working hours on working days
+else:
+    if clock_status != "working":
+        with open(status_file, "w") as f:
+            f.write("working")
+
+# Format the hour correctly for output
+if hour24 < 12:
+    hour = hour24 if mins < 35 else hour24 + 1
+elif hour24 == 12:
+    hour = hour24 if mins < 35 else 1
+else:
+    hour = hour24 - 12 if mins < 35 else hour24 - 11
+
+# Format the basic time output
+time1 = ''
+time2 = 'past' if mins < 35 else 'to'
+time3 = inflection.number_to_words(hour)
+
+# Break the minutes into 5 minute blocks
+if mins < 5:
+    time1 = 'bang on'
+    time2 = inflection.number_to_words(hour)
+    time3 = "o'clock"
+elif 5 <= mins < 10 or mins >= 55:
+    time1 = 'five'
+elif 10 <= mins < 15 or 50 <= mins < 55:
+    time1 = 'ten'
+elif 15 <= mins < 20 or 45 <= mins < 50:
+    time1 = 'a quarter'
+elif 20 <= mins < 25 or 40 <= mins < 45:
+    time1 = 'twenty'
+elif 25 <= mins < 30 or 35 <= mins < 40:
+    time1 = 'twenty-five'
+else:
+    time1 = 'half'
+
+# Format the time for display
+alignText("l", "t", "It's")
+alignText("m", "t", time1)
+alignText("m", "m", time2)
+alignText("m", "b", time3)
+alignText("r", "b", 'ish')
+
+# Update the display
+board.set_image(img)
+board.show()
+
+print("It's " + time1 + " " + time2 + " " + time3 + " ish")
